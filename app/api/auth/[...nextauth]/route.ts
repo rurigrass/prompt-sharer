10d2e6528450c03db4eb21550731f6b2b570be09
rurigrass <ruripg@gmail.com>
2023-05-16 14:29:33 +0200

    mongo db login is working now

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,12 @@ import GoogleProvider from "next-auth/providers/google";
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
+type ProfileProps = {
+  email: string;
+  name: string;
+  picture: string;
+};
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -11,30 +17,32 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      //serverless -> lambda ->
-      await connectToDB();
-      //check if a user already exists
-      const userExists = await User.findOne({ email: profile.email });
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }: {profile: ProfileProps}) {
+      try {
+        //serverless -> lambda ->
+        await connectToDB();
+        //check if a user already exists
+        const userExists = await User.findOne({ email: profile.email });
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error: any) {
+        console.log("theres an error in route.ts ", error.message);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
