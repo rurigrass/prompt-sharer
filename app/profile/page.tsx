e2d1cba351daa2a2fd07b44c35d298a972943003
rurigrass <ruripg@gmail.com>
2023-05-18 18:59:40 +0200

    delete functionality works

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -25,15 +25,7 @@ const MyProfile = () => {
   const { data: session } = useSession();
   const router = useRouter();
   const [prompts, setPrompts] = useState([]);
-
-  const handleEdit = (prompt: PromptProps) => {
-    console.log("EDIT ", prompt);
-    router.push(`/update-prompt?id=${prompt._id}`);
-  };
-
-  const handleDelete = (prompt: PromptProps) => {
-    console.log("DELETE prompt", prompt);
-  };
+  console.log(prompts);
 
   useEffect(() => {
     const fetchPrompts = async () => {
@@ -43,6 +35,31 @@ const MyProfile = () => {
     };
     if (session?.user?.id) fetchPrompts();
   }, []);
+
+  const handleEdit = (prompt: PromptProps) => {
+    console.log("EDIT ", prompt);
+    router.push(`/update-prompt?id=${prompt._id}`);
+  };
+
+  const handleDelete = async (prompt: PromptProps) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this Prompt?"
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${prompt._id.toString()}`, {
+          method: "DELETE",
+        });
+        const filteredPrompts = prompts.filter(
+          (item) => item._id !== prompt._id
+        );
+        setPrompts(filteredPrompts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    console.log("DELETE prompt", prompt);
+  };
 
   return (
     <Profile
